440c1e20e776c9389b7ec13507506ffc129deff8
Emily Tumbaco <s1167106@monmouth.edu>
2019-09-16 20:57:04 -0400

    Variables Created

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,21 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 * This program is will calculate the cost to paint a house  
+		 */
+		double houseLengthFeet;
+		double houseWidthFeet;
+		double houseHeightFeet; 
+		double windowLengthFeet;
+		double windowWidthFeet;
+		int windownumber;
+		double doorLengthFeet;
+		double doorWidthFeet;
+		int doornumber;
+		double inches;
+		
+	}
+
+}
