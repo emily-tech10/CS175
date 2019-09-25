1eedb6ffb14bbac15e7b83d3ed209b87f96e4bcd
Emily Tumbaco <s1167106@monmouth.edu>
2019-09-25 19:53:05 -0400

    Final Version of House Painting Project

--- HousePainting/src/PaintCost.java
+++ HousePainting/src/PaintCost.java
@@ -1,21 +1,74 @@
-
+import java.util.Scanner;
 public class PaintCost {
 
 	public static void main(String[] args) {
 		/*
-		 * This program is will calculate the cost to paint a house  
-		 */
-		double houseLengthFeet =   
-		double houseWidthFeet;
-		double houseHeightFeet; 
-		double windowLengthFeet;
-		double windowWidthFeet;
-		int windownumber;
-		double doorLengthFeet;
-		double doorWidthFeet;
-		int doornumber;
-		double inches;
-		double feet;
+		 * This program will calculate the cost to paint a house  
+		 */		
+		Scanner in = new Scanner (System.in); 
+		
+		System.out.print("Please enter the length of the house in feet : ");
+		double houseLengthFeet = in.nextDouble(); 
+		
+		
+		System.out.print("Please enter the width of the house in feet : ");
+		double houseWidthFeet = in.nextDouble(); 
+		
+		
+		System.out.print("Please enter the height of the house in feet : ");
+		double houseHeightFeet = in.nextDouble();
+		
+				
+		System.out.print("Please enter the number of windows in the house : ");
+		int numberOfWindows = in.nextInt();
+		
+		
+		System.out.print("Please enter the length of the window in feet : ");
+		double windowLengthFeet = in.nextDouble();
+		
+		
+		System.out.print("Please enter the width of the window in feet : ");
+		double windowWidthFeet = in.nextDouble();
+
+		
+		System.out.print("Please enter the number of doors in the house : ");
+		int numberOfDoors = in.nextInt();
+		
+		System.out.print("Please enter the length of the door : ");
+		double doorLengthFeet = in.nextDouble();
+		
+		
+		System.out.print("Please enter the width of the door in feet ");
+		double doorWidthFeet = in.nextDouble();
+		
+		double sqFTToPaint = 2756;
+		
+		System.out.println("Your total sq ft is " + sqFTToPaint);
+		
+		double costPerSqFt = 5.00;
+		
+		System.out.println("Your total cost to paint is " + sqFTToPaint*costPerSqFt);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+				
+				
+		
+				
+				
+		
+		
+		
+	
+		
 		
 	}
 
